0caef5e22eca5675358b24f617a05ba39d5737af
Дмитрий <deman7352@yandex.ru>
2024-06-26 17:13:08 +0300

    26.06beforeStart

---
+++ services/host/src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+
+export const App = () => {
+
+    return (
+        <div data-testid={'App.DataTestId'}>
+            <h1>PAGE</h1>
+        </div>
+    );
+};
+
+export default App;
